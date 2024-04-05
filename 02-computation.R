b0babca2d12db44eb9ac85e45841745484623bb2
George Kinnear <G.Kinnear@ed.ac.uk>
2024-04-05 13:58:09 +0100

    compute mcalc measure

--- 02-computation.R
+++ 02-computation.R
@@ -199,11 +199,130 @@ split_halves_data <- tibble(path = fs::dir_ls("data-cache", recurse = TRUE, glob
 # keep a cached copy of the split_corr and ssr_x for each split half
 split_halves_data %>% select(-G_x) %>% write_csv("data-cache/split_halves_summary_stats.csv")
 
-# summarise the splot_corr and ssr_x using medians
+# summarise the split_corr and ssr_x using medians
 split_halves_values <- split_halves_data %>% 
   group_by(judging_session) %>%
   summarise(median_split_corr = median(split_corr),
             median_ssr_x = median(ssr_x))
+
+
+
+#### Monte Carlo Alpha-Like Coefficient (MCALC) ####
+# 
+
+#1. helper function ----
+
+generate_mcalc <- function(csv_contents, judging_session, seed = 123, splits = 100, ...) {
+  print(str_glue("{judging_session} MCALC..."))
+  
+  # make sure the cache folder exists for this judging session
+  dir.create(file.path("data-cache", judging_session), showWarnings = FALSE)
+  splits_folder <- str_glue("data-cache/{judging_session}/mcalc/")
+  dir.create(file.path(splits_folder), showWarnings = FALSE)
+  
+  # read the number of cached results
+  existing_files <- fs::dir_ls(path = splits_folder,
+                               glob = "*.csv")
+  existing_splits <- parse_number(existing_files %>% str_remove(splits_folder))
+  splits_still_to_do <- setdiff(c(1:splits), existing_splits)
+  if(length(splits_still_to_do) == 0) {
+    print(str_glue("  ✔ all done"))
+  } else {
+    print(str_glue("  Splits still to do: {length(splits_still_to_do)}"))
+  }
+  
+  # format the judgements in the way that sirt::btm expects
+  judgements_for_sirt <- csv_contents %>%
+    select(judge, won, lost) %>% 
+    mutate(winner = 1) %>% # sirt's way to indicate the leftmost column is the winner
+    data.frame
+  
+  # make as many more splits as necessary to get up to the desired `splits` number
+  for (i in splits_still_to_do) {
+    print(str_glue("  Split {i}"))
+    # Set the seed for this split
+    set.seed(seed + i)
+    
+    # Group 1: sample the judges at random
+    judge_group1 <- judgements_for_sirt %>%
+      select(judge) %>%
+      distinct() %>%
+      slice_sample(prop = 0.5)
+    # Group 2: the remaining judges
+    judge_group2 <- judgements_for_sirt %>%
+      select(judge) %>%
+      distinct() %>%
+      anti_join(judge_group1, by = c("judge"))
+    
+    # Run BTM
+    judgements1 <- judgements_for_sirt %>% slice_sample(prop = 1, replace = TRUE) %>% select(-judge)
+    judgements2 <- judgements_for_sirt %>% slice_sample(prop = 1, replace = TRUE) %>% select(-judge)
+    
+    btm1 <- purrr::quietly(sirt::btm)(judgements1 %>% data.frame , maxit=400 , fix.eta=0 , ignore.ties=TRUE )$result
+    btm2 <- purrr::quietly(sirt::btm)(judgements2 %>% data.frame , maxit=400 , fix.eta=0 , ignore.ties=TRUE )$result
+    
+    merged_effects <- merge(btm1$effects, btm2$effects, by="individual")
+    
+    # Save results
+    write_csv(merged_effects, file = str_glue("data-cache/{judging_session}/mcalc/split{i}.csv"))
+  }
+}
+
+#2. generate the simulated datasets ----
+
+pmap_dfr(judgement_data_tidy %>% filter(!is.na(observed_N_A)), generate_mcalc, splits = 150)
+
+#3. read the cached results ----
+
+compute_mcalc_cors_from_cache <- function(judging_session, ...) {
+  fs::dir_ls(str_glue("data-cache/{judging_session}/mcalc/")) %>% 
+    vroom::vroom(id = "path", show_col_types = FALSE) %>% 
+    nest(data = !path) %>% 
+    mutate(mcalc_corr = map_dbl(data, ~ cor(.x$theta.x, .x$theta.y, method = "pearson"))) %>% 
+    # compute the sepG and SSR values for one of the split halves, to produce ssr_x
+    mutate(G_x = map_dbl(data, ~ sd(.x$theta.x) / sqrt(mean(.x$se.theta.x ^ 2)) ),
+           ssr_x = G_x ^ 2 / (1 + G_x ^ 2)
+    ) %>% 
+    select(-data) %>% 
+    write_csv(str_glue("data-cache/{judging_session}/mcalc.csv"))
+}
+
+# if you just want to do one ad-hoc:
+compute_mcalc_cors_from_cache(judging_session = "Bisson2016_algebra")
+
+# Loop through all the judging sessions and generate the mcalc.csv if it doesn't exist yet
+judgement_data_tidy %>% 
+  pwalk(function(...) {
+    args <- list(...) # get the list of named arguments
+    print(str_glue("{args$judging_session}"))
+    if(is.na(args$observed_N_A)) {
+      print(str_glue("  no judgement data yet"))
+    } else {
+      if(file.exists(str_glue("data-cache/{args$judging_session}/mcalc.csv"))) {
+        print(str_glue("  ✔ already done"))
+      } else {
+        compute_mcalc_cors_from_cache(judging_session = args$judging_session)
+        print(str_glue("  ✔✔ done"))
+      }
+    }
+  })
+
+mcalc_data <- tibble(path = fs::dir_ls("data-cache", recurse = TRUE, glob = "*mcalc.csv")) %>% 
+  separate(path, into = c(NA, "judging_session", NA), sep = "/", remove = FALSE) %>% 
+  mutate(mcalc_corr = map(path, vroom::vroom, delim = ",", show_col_types = FALSE)) %>% 
+  select(-path) %>% 
+  unnest(cols = mcalc_corr) %>% 
+  separate(path, into = c(NA, "iteration"), sep = "mcalc/split") %>% 
+  mutate(iteration = str_remove(iteration, ".csv"))
+
+# keep a cached copy of the mcalc_corr and ssr_x for each split
+mcalc_data %>% select(-G_x) %>% write_csv("data-cache/mcalc_summary_stats.csv")
+
+# summarise mcalc using medians
+mcalc_values <- mcalc_data %>% 
+  group_by(judging_session) %>%
+  summarise(median_mcalc_corr = median(mcalc_corr))
+
 
 
 #### EloChoice ####
@@ -388,3 +507,17 @@ ssr_values %>%
   left_join(pairs_values, by = "judging_session") %>% 
   left_join(prop_correct, by = "judging_session") %>% 
   write_csv("data/01-meta-analysis-data.csv")
+
+meta_analysis_data %>% 
+  left_join(mcalc_values) %>% 
+  ggplot(aes(x = median_ssr_x, y = median_mcalc_corr)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0)
+
+mcalc_data %>% 
+  group_by(judging_session) %>% 
+  filter(cur_group_id() <= 16) %>% 
+  ungroup() %>% 
+  ggplot(aes(x = ssr_x, y = mcalc_corr)) +
+  geom_point() +
+  facet_wrap(~ judging_session)
